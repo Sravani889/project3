fb529809a217f5d16466e05a8c21cdc031998b3a
sravani889 <vennapusalasravani2001@gmail.com>
2023-11-30 19:56:36 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sravani(request):
+    return render(request,'sravani.html')
+def abc(request):
+     return render(request,'sravani1.html')
+def shwetha(request):
+      return render(request,'sravani2.html')
+def ashwini(request):
+      return render(request,'sravani3.html')
+
+
